429adcdc70ece91f7d430168db3eb37e6fe2f80a
Robin Dahlberg <robin@forwarddevelopment.se>
2020-07-16 14:33:40 +0200

    added more availablePropTypes

--- web/src/components/SuperComponent.js
+++ web/src/components/SuperComponent.js
@@ -7,11 +7,6 @@ class SuperComponent extends React.Component {
       this.propValidation = propValidation;
     }
 
-/*    availablePropTypes = {
-        'float' : n => Number(n) === n && n % 1 !== 0,
-        'string' : s => typeof s === 'string' 
-    }*/
-
     validatePropTypes() {
         let isValid = true;
 
@@ -21,23 +16,6 @@ class SuperComponent extends React.Component {
                 console.warn(`${this.constructor.name}: ${keyName} did not comply with the validation ${validationFunction}`);
                 isValid = false;
             }
-            /*if(validationFunction instanceof RegExp && !this.props[keyName].match(validationFunction)) {
-                isValid = false;
-                console.warn(`${this.constructor.name}: ${keyName} did not match pattern of ${validationFunction}`);
-            }
-            else if(!(this.props[keyName] instanceof validationFunction))
-            {
-                isValid = false;
-                console.warn(`${this.constructor.name}: ${keyName} is not of the same instance as ${validationFunction}`);
-            }
-            else if(typeof validationFunction === 'function')
-            {
-                if(!validationFunction(this.props[keyName]))
-                {
-                    isValid = false;
-                    console.warn(`${this.constructor.name}: ${keyName} is invalid`);
-                }
-            }*/
         }
 
         return isValid;
@@ -45,8 +23,10 @@ class SuperComponent extends React.Component {
 }
 
 SuperComponent.availablePropTypes = {
-    'float' : n => Number(n) === n && n % 1 !== 0,
-    'string' : s => typeof s === 'string' 
+    'float' : () => ['float', n => Number(n) === n && n % 1 !== 0],
+    'string' : () => ['string', s => typeof s === 'string'],
+    'choice' : b => [b.split('|'), c => b.includes(c)],
+    'range' : (max, min) => [min + ' >= x =< ' + max, d => d <= max && d >= min]
 };
 
 export default SuperComponent;
