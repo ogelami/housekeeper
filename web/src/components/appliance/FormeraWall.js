6ff8ba3f8a52d864b79d82822b8be1ffbcbd0869
ogelami <ogelami@gmail.com>
2022-01-17 11:03:16 +0100

    FormeraWall appliance is now now using the new structure.

--- web/src/components/appliance/FormeraWall.js
+++ web/src/components/appliance/FormeraWall.js
@@ -1,10 +1,10 @@
 import ToggleSwitch from '../ToggleSwitch';
 
-class FormeraWall extends ToggleSwitch {
-  icon = ['mdi-lightbulb-on-outline', 'mdi-lightbulb-outline'];
-  sendOn = '1';
-  sendOff = '0';
-  receiveOn = '1';
-}
-
-export default FormeraWall;
+export default function FormeraWall(props) {
+  return ToggleSwitch({...{
+    icon : ['mdi-lightbulb-on-outline', 'mdi-lightbulb-outline'],
+    sendOn : '1',
+    sendOff : '0',
+    receiveOn : '1'
+  }, ...props});
+};
